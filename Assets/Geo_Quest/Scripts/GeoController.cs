7b51f4d1474ec73fdbdf368137f23533551a0d01
k0z17 <dflores4208@gmail.com>
2025-07-09 12:05:19 -0400

    update

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+   
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        
+    }
+    int vartwo = 3;
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(vartwo);
+        vartwo++;
+    }
+}
