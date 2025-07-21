f55272f254378bbf06310243b127b448737aa209
k0z17 <dflores4208@gmail.com>
2025-07-21 11:54:34 -0400

    update

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
     // Capsule
-    public float CapsuleHeigh = 0.25f;
+    public float CapsuleHeight = 0.25f;
     public float CapsuleRadius = 0.08f;
     // Ground Check
 
@@ -15,12 +16,14 @@ public class PlayerJump : MonoBehaviour
     // Components
     private Rigidbody2D _rigidbody2D;
     // Forces
-    public float jumpForce = 10;
-
+    public float jumpForce = 10f;
+    public float fallForce = -1;
+    private Vector2 gravityForce;
     // Sets gravity vector and connects components
 
     void Start()
     {
+        gravityForce = new Vector2(0f, Physics2D.gravity.y);
         _rigidbody2D = GetComponent<Rigidbody2D>();
 
 
@@ -31,12 +34,44 @@ public class PlayerJump : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        _groundCheck = Physics2D.OverlapCapsule(point: feetCollider.position, size: new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Vertical, angle: 0, groundMask);
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && _groundCheck)
         {
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce);
 
 
         }
+        if (_rigidbody2D.velocity.y > 0)
+        {
+            _rigidbody2D.velocity += gravityForce * (fallForce * Time.deltaTime);
+
+
+
+        }
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
 
 
